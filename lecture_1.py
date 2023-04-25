6a6b858748aa419d5e0152a6cff4fafef2f281bf
Mihail <mohonovproduction@yandex.ru>
2023-04-25 10:39:06 +0300

    1 task

---
+++ lecture_1.py
@@ -0,0 +1,18 @@
+# no more 10 elements, no bigger then 4096
+
+# Task 1
+
+second_degrees = list()
+for i in range(1, 20):
+    second_degrees.append(2 ** i)
+
+print(second_degrees)
+
+
+# Task 2
+
+# Task 3
+
+# Task 4
+
+# Task 5 (Homework)
