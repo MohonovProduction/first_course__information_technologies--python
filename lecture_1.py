849594c394a0df4c0d71a7105515649fd0e3c954
Mihail <mohonovproduction@yandex.ru>
2023-04-25 10:43:01 +0300

    task 2

--- lecture_1.py
+++ lecture_1.py
@@ -6,10 +6,21 @@ second_degrees = list()
 for i in range(1, 20):
     second_degrees.append(2 ** i)
 
-print(second_degrees)
+# print(second_degrees)
 
 
 # Task 2
+
+div_5 = list()
+for i in range(1, 10):
+    number = 5 * i + 3
+    div_5.append(number)
+
+print(div_5)
+div_5.reverse()
+
+print(div_5)
+
 
 # Task 3
 
